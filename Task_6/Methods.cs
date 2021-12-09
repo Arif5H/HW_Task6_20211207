0e21acfe1f105bbef7d8356eb53ed2c3cda83872
Arif5H <arifhh@code.edu.az>
2021-12-09 20:36:32 +0400

    Добавьте файлы проекта.

---
+++ Task_6/Methods.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+   internal class Methods
+    {
+        public string username;
+        public string password;
+
+        public string UserName
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {  
+                username = value;
+                if (username.Length < 5)
+                {
+                    throw new Exception($"Username {username} should consist at least of 5 symbols");
+                    
+                }
+            }
+        }public string Password
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                password = value;
+                if (password.Length < 8)
+                {
+                    throw new Exception($"Password {password} should consist as least of 8 symbols");
+
+                }
+            }
+        }
+
+        public bool Login()
+        {   
+            if (username == "ArifHH" && password == "F14us4Ever" )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+       
+
+    }
+}
---
+++ Task_6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Username: ArifHH
+        //Password: F14us4Ever               
+            Methods entrycheck = new Methods();
+            step1:
+            try
+            {
+                Console.Write("Username:  ");
+                entrycheck.UserName = Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                goto step1;
+            }
+            step2:
+            try
+            {
+                Console.Write($"Password  of {entrycheck.username}:  ");
+                entrycheck.Password = Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                goto step2;
+            }
+
+                bool isLoggedin = entrycheck.Login();
+
+                if (isLoggedin)
+                {
+                    Console.WriteLine("You are LOGGED IN");
+                }
+                else
+                {
+                    Console.WriteLine("TRY AGAIN");
+                    goto step1;
+                }
+      
+
+
+        }
+            
+            
+    }
+}
+
